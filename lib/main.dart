9bb4007730ddf4c2ed30e8df161b45a40b96036a
colombiaje <colombiaje@gmail.com>
2025-04-18 09:14:38 -0500

    Primer commit con estructura correcta

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/prompt_form.dart';
+
+void main() {
+  runApp(const ZtorePromptApp());
+}
+
+class ZtorePromptApp extends StatelessWidget {
+  const ZtorePromptApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ZtorePrompt',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: PromptFormScreen(),
+    );
+  }
+  //foco 1
+}
